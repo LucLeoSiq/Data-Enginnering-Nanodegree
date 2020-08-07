e07198712bba3443a1c613190c1ca45aaed65d2d
Lucas Leonel Siqueira <luleosiq@gmail.com>
2020-08-07 08:29:39 -0300

    Define operator parameters

--- P4_dend_Pipelines_Airflow/plugins/operators/stage_redshift.py
+++ P4_dend_Pipelines_Airflow/plugins/operators/stage_redshift.py
@@ -2,14 +2,22 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+# The stage operator is expected to be able to load any JSON formatted files
+# from S3 to Amazon Redshift. The operator creates and runs a SQL COPY statement
+# based on the parameters provided. The operator's parameters should specify
+# where in S3 the file is loaded and what is the target table.
+
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 delimiter=",",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
@@ -19,8 +27,3 @@ class StageToRedshiftOperator(BaseOperator):
 
     def execute(self, context):
         self.log.info('StageToRedshiftOperator not implemented yet')
-
-
-
-
-
