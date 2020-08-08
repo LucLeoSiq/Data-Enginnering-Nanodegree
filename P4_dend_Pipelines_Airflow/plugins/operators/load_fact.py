26d9392cc38e50331383b2daafb37e8547973f35
Lucas Leonel Siqueira <luleosiq@gmail.com>
2020-08-08 16:11:25 -0300

    Add comment explaining purpose of the "LoadFactOperator" operator

--- P4_dend_Pipelines_Airflow/plugins/operators/load_fact.py
+++ P4_dend_Pipelines_Airflow/plugins/operators/load_fact.py
@@ -2,6 +2,13 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+# With fact operator, you can utilize the provided SQL helper class to run data
+# transformations. Most of the logic is within the SQL transformations and the
+# operator is expected to take as input a SQL statement and target database on
+# which to run the query against. You can also define a target table that will
+# contain the results of the transformation.
+
+
 class LoadFactOperator(BaseOperator):
 
     ui_color = '#F98866'
