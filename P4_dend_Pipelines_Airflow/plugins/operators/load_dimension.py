0edce24016dc8b031ac696f78814cab62b7b2d00
Lucas Leonel Siqueira <luleosiq@gmail.com>
2020-08-08 19:19:16 -0300

    Add comment detailing the "LoadDimensionOperator" operator

--- P4_dend_Pipelines_Airflow/plugins/operators/load_dimension.py
+++ P4_dend_Pipelines_Airflow/plugins/operators/load_dimension.py
@@ -2,6 +2,15 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+# With dimension operator, utilize the provided SQL helper class to run data
+# transformations. Most of the logic is within the SQL transformations and the
+# operator is expected to take as input a SQL statement and target database on
+# which to run the query against. You can also define a target table that will
+# contain the results of the transformation.
+
+# Dimension loads are often done with the truncate-insert pattern where the
+# target table is emptied before the load.
+
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
