45c967c92af910a19d5594627eea0a757e7d3c99
Lucas Leonel Siqueira <luleosiq@gmail.com>
2020-08-09 10:32:21 -0300

    Add comment detailing functionality of "DataQualityOperator" operator

--- P4_dend_Pipelines_Airflow/plugins/operators/data_quality.py
+++ P4_dend_Pipelines_Airflow/plugins/operators/data_quality.py
@@ -2,6 +2,14 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+# The data quality operator, which is used to run checks on the data itself.
+# The operator's main functionality is to receive one or more SQL based test
+# cases along with the expected results and execute the tests.
+
+# For each the test, the test result and expected result needs to be checked
+# and if there is no match, the operator should raise an exception and the
+# task should retry and fail eventually.
+
 class DataQualityOperator(BaseOperator):
 
     ui_color = '#89DA59'
