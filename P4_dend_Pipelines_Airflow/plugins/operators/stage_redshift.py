a19629e9dc80e031676ecff1ce8dfbf1c4b1f466
Lucas Leonel Siqueira <luleosiq@gmail.com>
2020-08-10 21:39:16 -0300

    Adjust execute function

--- P4_dend_Pipelines_Airflow/plugins/operators/stage_redshift.py
+++ P4_dend_Pipelines_Airflow/plugins/operators/stage_redshift.py
@@ -44,24 +44,26 @@ class StageToRedshiftOperator(BaseOperator):
         self.table = table
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
-        self.delimiter = delimiter
+        self.extra_params=extra_params
 
     def execute(self, context):
         aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
+       # self.log.info("Clearing data from destination Redshift table")
+       # redshift.run(f"DELETE FROM {self.table}")
+        
         self.log.info("Copying data from S3 to Redshift")
         rendered_key = self.s3_key.format(**context)
         s3_path = f"s3://{self.s3_bucket}/{rendered_key}"
 
-        formatted_sql = S3ToRedshiftOperator.copy_sql.format(
+        formatted_sql = StageToRedshiftOperator.copy_sql.format(
             self.table,
             s3_path,
             credentials.access_key,
             credentials.secret_key,
-            self.ignore_headers,
-            self.delimiter
+            self.extra_params
         )
 
         self.log.info(f"Executing query to copy data from '{s3_path}' to '{self.table}'")
